b3336b047170c4ab3d95d9b34b45ead6fc34e607
Hamatsuu <hamatsu17@gmail.com>
2026-05-15 13:36:33 -0600

    Actualizar bundle, reflejar precio si ya hay juegos en libreria

--- model/bundles/Bundle.java
+++ model/bundles/Bundle.java
@@ -2,9 +2,9 @@ package model.bundles;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import model.games.Juego;
 import model.store.ArticuloComprable;
+import repository.DataStore;
 
 public class Bundle implements ArticuloComprable {
 
@@ -32,17 +32,37 @@ public class Bundle implements ArticuloComprable {
 
     @Override
     public double getPrecio() {
-        double total = 0;
+        return getPrecioSinDescuento() * (1 - descuento);
+    }
 
+    public double getPrecioSinDescuento() {
+        double total = 0;
         for (Juego j : juegos) {
             total += j.getPrecio();
         }
-
-        return total * (1 - descuento);
+        return total;
     }
+
+    public double getDescuento() {
+        return descuento;
+    }
+
+    public double getPrecioPara(model.users.Jugador jugador) {
+        double total = getPrecioSinDescuento();
+        double ownedTotal = 0;
+        for (Juego j : juegos) {
+            if (jugador.getLibreria().getItems().contains(j)) {
+                ownedTotal += j.getPrecio();
+            }
+        }
+        double remaining = total - ownedTotal;
+        return remaining <= 0 ? 0 : remaining * (1 - descuento);
+    }
+
     @Override
     public String toString(){
-        return nombre + "Bundle - $" + getPrecio();
+        double precio = DataStore.currentPlayer != null ? getPrecioPara(DataStore.currentPlayer) : getPrecio();
+        return nombre + " Bundle - $" + String.format("%.2f", precio);
     }
 }
 
