377b58b3ddc5b85224023b14aae84146c750db74
hamatsuu <hamatsu17@gmail.com>
2026-05-14 22:19:18 -0600

    x

---
+++ model/users/Usuario.java
@@ -0,0 +1,27 @@
+// Clase abstracta usuario
+// Dependencia UserFactory
+// debería implementar entre algunas cosas lo siguiente:
+// email
+// username
+// password
+
+package model.users;
+
+public abstract class Usuario {
+
+    protected String username;
+    protected String email;
+
+    public Usuario(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
