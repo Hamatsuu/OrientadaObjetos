e9a22609998c4a6ff82abd8c53cd24ab6c5648f2
Hamatsuu <hamatsu17@gmail.com>
2026-05-15 13:38:38 -0600

    Bundle cuando ya tienes todos los juegos

--- service/StoreService.java
+++ service/StoreService.java
@@ -1,5 +1,6 @@
 package service;
 
+import model.bundles.Bundle;
 import model.store.ArticuloComprable;
 import model.users.Jugador;
 
@@ -8,15 +9,23 @@ public class StoreService {
     // Cambiamos el boolean por void y agregamos 'throws Exception'
     public void purchase(Jugador jugador, ArticuloComprable item) throws Exception {
 
-        // 1. Validar si el jugador ya tiene el artículo
+        // 1. Validar si el jugador ya tiene el artículo exacto
         if (jugador.getLibreria().getItems().contains(item)) {
-            // Lanzamos el error hacia la UI
             throw new Exception("Intento de compra denegado: Ya tienes " + item.getNombre() + " en tu librería.");
         }
 
+        double precio = item.getPrecio();
+
+        if (item instanceof Bundle) {
+            Bundle bundle = (Bundle) item;
+            precio = bundle.getPrecioPara(jugador);
+            if (precio <= 0) {
+                throw new Exception("Ya posees todos los juegos de este bundle.");
+            }
+        }
+
         // 2. Intentar descontar el saldo
-        if (!jugador.withdraw(item.getPrecio())) {
-            // Si withdraw devuelve false, no hay dinero
+        if (!jugador.withdraw(precio)) {
             throw new Exception("Saldo insuficiente para comprar " + item.getNombre() + ".");
         }
 
